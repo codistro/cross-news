8573091d69b90ad9bf61033a843f38dfc29ad216
Rajan Choudhary <rajach19@in.ibm.com>
2019-04-23 17:43:32 +0530

    added api call and showing news in cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'dart:convert';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
 
-  final List<String> list = List.generate(1000, (index) => 'News $index');
+class _MyAppState extends State<MyApp> {
+  List<dynamic> list = [];
+
+  void getNews() async {
+    var response = await http.get(
+        "https://newsapi.org/v2/top-headlines?country=in&apiKey=95969a234afd445182b47718844222df");
+
+    Map<String, dynamic> result = json.decode(response.body);
+    print('i am in ${result["articles"]}');
+    setState(() {
+      list = result["articles"];
+    });
+  }
+
+  @override
+  void initState() {
+    getNews();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Top 10 News'),
+          title: Text('Top News'),
         ),
-        body: ListView.builder(itemBuilder: (context, index) {
-          return Text(list[index]);
-        }),
+        body: ListView.builder(
+          itemBuilder: (context, index) {
+            return Card(
+              child: Column(
+                children: <Widget>[
+                  list[index]['urlToImage'] != null
+                      ? Image.network(list[index]['urlToImage'])
+                      : Icon(Icons.hourglass_empty),
+                  Container(
+                    child: Text(
+                      list[index]['title'],
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    margin: EdgeInsets.all(10.0),
+                  ),
+                ],
+              ),
+            );
+          },
+          itemCount: list.length,
+          padding: EdgeInsets.all(6.0),
+        ),
       ),
     );
   }
