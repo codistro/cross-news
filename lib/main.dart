6caebc7bddd6d27eadc13d6b60f8817f9608f507
Rajan Choudhary <rajach19@in.ibm.com>
2019-04-24 15:29:03 +0530

    Added RefreshIndicator and open url in browser

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart';
 
 import 'dart:convert';
 
@@ -15,15 +16,23 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   List<dynamic> list = [];
 
-  void getNews() async {
+  Future<Null> getNews() async {
     var response = await http.get(
         "https://newsapi.org/v2/top-headlines?country=in&apiKey=95969a234afd445182b47718844222df");
 
     Map<String, dynamic> result = json.decode(response.body);
-    print('i am in ${result["articles"]}');
     setState(() {
       list = result["articles"];
     });
+    return;
+  }
+
+  _launchURL(url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 
   @override
@@ -35,33 +44,43 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Text('Top News'),
           backgroundColor: Colors.red,
         ),
-        body: ListView.builder(
-          itemBuilder: (context, index) {
-            return Card(
-              child: Column(
-                children: <Widget>[
-                  list[index]['urlToImage'] != null
-                      ? Image.network(list[index]['urlToImage'])
-                      : Icon(Icons.hourglass_empty),
-                  Container(
-                    child: Text(
-                      list[index]['title'],
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    margin: EdgeInsets.all(10.0),
+        body: RefreshIndicator(
+          onRefresh: getNews,
+          child: ListView.builder(
+            itemBuilder: (context, index) {
+              return Card(
+                child: GestureDetector(
+                  onTap: (){
+                    var url = list[index]['url'];
+                    _launchURL(url);
+                  },
+                  child: Column(
+                    children: <Widget>[
+                      list[index]['urlToImage'] != null
+                          ? Image.network(list[index]['urlToImage'])
+                          : Icon(Icons.hourglass_empty),
+                      Container(
+                        child: Text(
+                          list[index]['title'],
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        margin: EdgeInsets.all(10.0),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            );
-          },
-          itemCount: list.length,
-          padding: EdgeInsets.all(6.0),
+                ),
+              );
+            },
+            itemCount: list.length,
+            padding: EdgeInsets.all(6.0),
+          ),
         ),
       ),
     );
